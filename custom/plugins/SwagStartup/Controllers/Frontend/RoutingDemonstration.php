4f3a4f33f4310bc544f9c4ab92522d8dc15262ae
Tuncer Damat <tuncer.damat@gmail.com>
2021-03-15 15:16:23 +0100

    Block, widgets & auhtentication - protecting a route with user authentication

--- custom/plugins/SwagStartup/Controllers/Frontend/RoutingDemonstration.php
+++ custom/plugins/SwagStartup/Controllers/Frontend/RoutingDemonstration.php
@@ -7,11 +7,24 @@ class Shopware_Controllers_Frontend_RoutingDemonstration extends Enlight_Control
 {
     public function preDispatch()
     {
-        $this->view->addTemplateDir(__DIR__ . '/../../Resources/views');
-
         $currentAction = $this
             ->request()
             ->getActionName();
+        
+        // If user is not looged in then user id is set to null
+        if (null === $this->get('session')->get('sUserId')
+            && 'index' === $currentAction) {
+            // Redirect to account controller
+            $this->redirect([
+               'module' => 'frontend',
+               'controller' => 'account',
+               'action' => 'login', 
+               'sTarget' => 'routing_demonstration', 
+               'sTargetAction' => 'index' 
+            ]);
+        }
+        
+        $this->view->addTemplateDir(__DIR__ . '/../../Resources/views');
 
         $this->view
             ->assign('currentAction', $currentAction);
--- custom/plugins/SwagStartup/Resources/views/frontend/routing_demonstration/index.tpl
+++ custom/plugins/SwagStartup/Resources/views/frontend/routing_demonstration/index.tpl
@@ -1,5 +1,7 @@
 {extends file="parent:frontend/index/index.tpl"}
 
+{block name="frontend_index_sidebar"}{/block}
+
 {block name='frontend_index_content'}
     <h1>Hello template inheritance</h1>
     <h1>{$currentAction} action template</h1>
@@ -7,4 +9,6 @@
     <a href="{url module='frontend' controller='routing_demonstration' action=$nextPage}">
         {s name="RoutingDemonstrationNextPage"}{/s}
     </a>
+    
+    {action module='widgets' controller='listing' action='topSeller'}
 {/block}
